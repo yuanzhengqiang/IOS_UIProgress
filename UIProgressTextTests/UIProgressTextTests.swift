0c07264db881287f2d789058c8ee6852e75f5dea
yuanzhengqiang <273816488@qq.com>
2017-03-31 16:25:28 +0800

    Initial Commit

---
+++ UIProgressTextTests/UIProgressTextTests.swift
@@ -0,0 +1,36 @@
+//
+//  UIProgressTextTests.swift
+//  UIProgressTextTests
+//
+//  Created by 原政强 on 17/3/31.
+//  Copyright © 2017年 yzq. All rights reserved.
+//
+
+import XCTest
+@testable import UIProgressText
+
+class UIProgressTextTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
