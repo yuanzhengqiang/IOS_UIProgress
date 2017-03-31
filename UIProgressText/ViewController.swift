cd46c2303ee225388dff1889568a73204b334f7c
yuanzhengqiang <273816488@qq.com>
2017-03-31 17:12:27 +0800

    进度条Demo

--- UIProgressText/ViewController.swift
+++ UIProgressText/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        progress1.progress = 0
+        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: changeProgress)
+    }
+    func changeProgress(sender:Timer) {
+        progress1.progress += 0.01
+        if (progress1.progress >= 1) {
+            sender.invalidate()
+        }
     }
 
+    @IBOutlet weak var progress1: UIProgressView!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
